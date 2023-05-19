2c5bf0646d2e3fa721e48ddbc92bc79a0f341c22
Naowak <yannis.bendiouis@gmail.com>
2023-05-19 23:34:00 +0200

    animate the movement up and down

--- game/src/components/Agent.js
+++ game/src/components/Agent.js
@@ -7,6 +7,7 @@ const Agent = ({ initialPosition, team, life, position }) => {
   const ref = useRef();  // Use useRef instead of useSphere
   const speed = 0.1;  // Adjust this value for different movement speeds
   const precision = 3;  // Number of decimal places for position coordinates
+  let upDown = 1;  // Used to animate the agent up and down
 
   useFrame(() => {
     if (ref.current) {
@@ -25,6 +26,11 @@ const Agent = ({ initialPosition, team, life, position }) => {
         ref.current.position.z + direction.z * speed
       );
 
+      // Apply up and down animation to the agent (depends on the speed animation)
+      if (ref.current.position.y >= 0.25) { upDown = -1; }
+      if (ref.current.position.y <= 0) { upDown = 1; }
+      nextPosition.y += 0.05 * upDown;
+
       // If nextPosition is close enough to the target position, just set it directly to the target position
       if (nextPosition.distanceTo(new THREE.Vector3(...position)) < speed) {
         ref.current.position.set(...position);
@@ -40,8 +46,8 @@ const Agent = ({ initialPosition, team, life, position }) => {
   });
 
   return (
-    <mesh ref={ref} position={initialPosition}>
-      <sphereBufferGeometry attach='geometry' args={[0.5, 32, 32]} />
+    <mesh ref={ref} position={[initialPosition[0], initialPosition[1], initialPosition[2]]}>
+      <sphereBufferGeometry attach='geometry' args={[0.3, 32, 32]} />
       <meshStandardMaterial attach='material' color={team === 'red' ? 'red' : 'blue'} />
     </mesh>
   );
