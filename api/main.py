e07d0428db8c380826d933b375f7d62a0b2af08b
Naowak <yannis.bendiouis@gmail.com>
2023-06-02 16:48:23 +0200

    wip for action reader

--- api/main.py
+++ api/main.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 # Load env variables
 load_dotenv()
 
+# Initialize the app
 app = Flask(__name__)
 CORS(app)
 
@@ -21,63 +22,29 @@ model = ChatOpenAI(
 )
 
 # Initialize the system message
-system_message = SystemMessage(
-    content=(
-        "You're an artificial intelligence in a strategic turn-based video game played on a checkerboard. \n"
-        "You take on the role of a character in this game, and are accompanied by other agents who, like you, move through the video game autonomously. \n"
-        "Each of you starts with 100 life points, and can perform 4 actions per turn, which are restricted to the following:\n"
-        "- move one square (up, down, right, left)\n"
-        "- attack a square (removes 25 life points from the target)\n"
-        "You are divided into two teams (red and blue).\n"
-        "Each team has a target (200 life points) which it must defend, and wins when it has destroyed the opposing target, or killed all the opposing agents. \n"
-        "\n"
-        "Based on the information we'll give you (your position and what you see in your field of view), you'll have to explain your situation, what you plan to do and why.\n"
-        "Then, based on these thoughts, you'll have to give us a sequence of 4 actions to carry out. Please note that each action has consequences, and is dependent on the previous actions.\n"
-        "\n"
-        "You can only attack squares that are in your field of view.\n"
-        "You can only move on empty squares.\n"
-        "When you choose to move, your position is modified as follows:\n"
-        "- MOVE UP => position + (0, -1)\n"
-        "- MOVE DOWN => position + (0, +1)\n"
-        "- MOVE LEFT => position + (-1, 0)\n"
-        "- MOVE UP => position + (+1, 0)\n"
-        "\n"
-        "Please answer strictly in the following format:\n"
-        "```\n"
-        "THOUGHTS:\n"
-        "[situation, plans, why]\n"
-        "\n"
-        "ACTIONS:\n"
-        "1. [action 1]\n"
-        "2. [action 2]\n"
-        "3. [action 3]\n"
-        "4. [action 4]\n"
-        "```\n"
-        "\n"
-        "Here is an example of what is expected for ACTIONS:\n"
-        "```\n"
-        "1. MOVE UP\n"
-        "2. MOVE DOWN\n"
-        "3. ATTACK [-1, 7]\n"
-        "4. ATTACK [-1, 7]\n"
-        "```\n"
-    )
-)
+system_txt = ""
+with open('./prompts/system_message.txt', 'r') as f:
+    system_txt = f.read()
+system_message = SystemMessage(content=(system_txt))
+
+
 
 # Parse the response into thoughts and action
 def read_answer(response):
     message = response.content
     try:
         lines = message.split('\n')
-        thoughts = lines[1]
-        actions = lines[4:8]
-        actions = [a[3:] for a in actions]
-        return thoughts, actions
+        thoughts = [l for l in lines if l.startswith('THOUGHTS: ')][0][10:]
+        action = [l for l in lines if l.startswith('ACTION: ')][0][8:]
+        return thoughts, action
     except:
         with open('response.txt', 'w') as f:
             f.write(response.content)
-        return "", ["", "", "", ""]
+        return "", ""
 
+
+
+# Define the API routes
 @app.route('/play_one_turn', methods=['POST'])
 def play():
 
@@ -91,13 +58,16 @@ def play():
     # Get the response from the model
     message = HumanMessage(content=str(request.json['state']))
     response = model([system_message, message])
-    thoughts, actions = read_answer(response)
+    thoughts, action = read_answer(response)
 
     # Return the thoughts and action
     return jsonify({
         'thoughts': thoughts,
-        'actions': actions
+        'action': action
     })
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- game/src/libs/play.js
+++ game/src/libs/play.js
@@ -2,6 +2,92 @@ import { NB_ACTIONS_PER_TURN, BOARD_SIZE } from './constants';
 import { handleMove, handleAttack } from './actions';
 
 
+// FUNCTIONS
+const getPossibleMoves = (agent) => {
+  const adjacantCells = [
+    [agent.position[0], agent.position[1] - 1],
+    [agent.position[0], agent.position[1] + 1],
+    [agent.position[0] - 1, agent.position[1]],
+    [agent.position[0] + 1, agent.position[1]],
+  ];
+  const possibleMoves = []
+  adjacantCells.forEach(cell => {
+    const free = agent.sight.find(o => o.position[0] === cell[0] && o.position[1] === cell[1]) === undefined;
+    const inBoard = cell[0] >= -BOARD_SIZE && cell[0] <= BOARD_SIZE && cell[1] >= -BOARD_SIZE && cell[1] <= BOARD_SIZE;
+    if (free && inBoard) possibleMoves.push(`MOVE [${cell[0]}, ${cell[1]}]`)
+  });
+  return possibleMoves;
+}
+
+const getPossibleAttacks = (agent) => {
+  return agent.sight.filter(o => ((o.kind === 'agents' || o.kind === 'targets') && o.team !== agent.team)).map(
+    o => `ATTACK [${o.position[0]}, ${o.position[1]}]`
+  );
+}
+
+// Function that converts an agent's sight and infos to a state
+const getAgentState = (agent, turn) => {
+
+  // Compute which movement are possible
+  const possibleMoves = getPossibleMoves(agent);
+
+  // Compute which cell he can attack
+  const possibleAttacks = getPossibleAttacks(agent);
+
+  // Create state
+  const state = {}
+  state['Your Position'] = agent.position;
+  state['Your Health'] = agent.life;
+  state['Friends'] = agent.sight.filter(o => o.kind === 'agents' && o.team === agent.team).map(
+    o => ({ position: o.position, health: o.life })
+  );
+  state['Enemies'] = agent.sight.filter(o => o.kind === 'agents' && o.team !== agent.team).map(
+    o => ({ position: o.position, health: o.life })
+  );
+  state['Friend Target'] = agent.sight.filter(o => o.kind === 'targets' && o.team === agent.team).map(
+    o => ({ position: o.position, health: o.life })
+  )
+  state['Enemy Target'] = agent.sight.filter(o => o.kind === 'targets' && o.team !== agent.team).map(
+    o => ({ position: o.position, health: o.life })
+  )
+  state['Obstacles'] = agent.sight.filter(o => o.kind === 'obstacles').map(o => o.position);
+  state['Turn'] = turn.current;
+  state['Actions Left'] = NB_ACTIONS_PER_TURN - turn.actions;
+  state['Possible Actions'] = [...possibleMoves, ...possibleAttacks];
+  return state;
+}
+
+const readAction = (action) => {
+
+  if (action.slice(0, 4) === 'MOVE') {
+
+    // Check if move is possible
+    const possibleMoves = getPossibleMoves(agent);
+    const cell = action.slice(6, -1).split(',').map(o => parseInt(o));
+    const possible = possibleMoves.find(o => o === action) !== undefined;
+
+    if (possible) {
+      return 'MOVE', cell;
+    }
+
+  } else if (action.slice(0, 6) === 'ATTACK') {
+
+    // Check if attack is possible
+    const possibleAttacks = getPossibleAttacks(agent);
+    const cell = action.slice(8, -1).split(',').map(o => parseInt(o));
+    const possible = possibleAttacks.find(o => o === action) !== undefined;
+
+    if (possible) {
+      return 'ATTACK', cell;
+    }
+  }
+
+  return null, null;
+}
+
+
+// EXPORTS
+
 // Function called when a key [up, down, left, right, space, enter] is pressed
 const playKeyboard = (event, waitingInput, turn, win, agents, targets, obstacles, setAgents, setBullets, animationQueue, setAnimationQueue, nextAction, newGame) => { 
   // Prevent multiple inputs
@@ -38,52 +124,6 @@ const playKeyboard = (event, waitingInput, turn, win, agents, targets, obstacles
   setTimeout(() => waitingInput.current = false, 500);
 };
 
-// Function that converts an agent's sight and infos to a state
-const getAgentState = (agent, turn) => {
-
-  // Compute which movement are possible
-  const adjacantCells = [
-    [agent.position[0], agent.position[1] - 1],
-    [agent.position[0], agent.position[1] + 1], 
-    [agent.position[0] - 1, agent.position[1]], 
-    [agent.position[0] + 1, agent.position[1]], 
-  ];
-  const possibleMoves = []
-  adjacantCells.forEach(cell => {
-    const free = agent.sight.find(o => o.position[0] === cell[0] && o.position[1] === cell[1]) === undefined;
-    const inBoard = cell[0] >= -BOARD_SIZE && cell[0] <= BOARD_SIZE && cell[1] >= -BOARD_SIZE && cell[1] <= BOARD_SIZE;
-    if (free && inBoard) possibleMoves.push(`MOVE [${cell[0]}, ${cell[1]}]`)
-  });
-
-  // Compute which cell he can attack
-  const possibleAttacks = agent.sight.filter(o => ((o.kind === 'agents' || o.kind === 'targets') && o.team !== agent.team)).map(
-    o => `ATTACK [${o.position[0]}, ${o.position[1]}]`
-  );
-
-  console.log(possibleMoves, possibleAttacks)
-
-  // Create state
-  const state = {}
-  state['Your Position'] = agent.position;
-  state['Your Health'] = agent.life;
-  state['Friends'] = agent.sight.filter(o => o.kind === 'agents' && o.team === agent.team).map(
-    o => ({ position: o.position, health: o.life })
-  );
-  state['Enemies'] = agent.sight.filter(o => o.kind === 'agents' && o.team !== agent.team).map(
-    o => ({ position: o.position, health: o.life })
-  );
-  state['Friend Target'] = agent.sight.filter(o => o.kind === 'targets' && o.team === agent.team).map(
-    o => ({ position: o.position, health: o.life })
-  )
-  state['Enemy Target'] = agent.sight.filter(o => o.kind === 'targets' && o.team !== agent.team).map(
-    o => ({ position: o.position, health: o.life })
-  )
-  state['Obstacles'] = agent.sight.filter(o => o.kind === 'obstacles').map(o => o.position);
-  state['Turn'] = turn.current;
-  state['Actions Left'] = NB_ACTIONS_PER_TURN - turn.actions;
-  state['Possible Actions'] = [...possibleMoves, ...possibleAttacks];
-  return state;
-}
 
 
 // Function called when key "A" is pressed, to play the AI
@@ -96,9 +136,6 @@ const playAI = async (turn, win, agents, targets, obstacles, setAgents, setBulle
 
   // Find current agent
   const currentAgent = agents.find(agent => agent.id === turn.agentId);
-  console.log(getAgentState(currentAgent, turn))
-
-  return
 
   // Update agent thinking 
   setAgents(agents.map(agent => {
@@ -159,6 +196,9 @@ const playAI = async (turn, win, agents, targets, obstacles, setAgents, setBulle
   if (animation) {
     setAnimationQueue([...animationQueue, animation]);
     nextAction();
+  }
+  else {
+    console.log('Invalid action', action);
   }
 
 };
