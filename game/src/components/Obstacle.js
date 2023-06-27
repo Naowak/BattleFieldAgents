290c027c431706242868169b912396fbb047b47d
Naowak <yannis.bendiouis@gmail.com>
2023-06-27 17:04:20 +0200

    reduce obstacle size on screen

--- game/src/components/Obstacle.js
+++ game/src/components/Obstacle.js
@@ -1,16 +1,16 @@
 import React, { useRef } from 'react';
 import * as THREE from 'three';
-import { OBSTACLE_TRANSLATE_Y, COLOR_OBSTACLE, COLOR_OBSTACLE_BORDER } from '../libs/constants';
+import { OBSTACLE_TRANSLATE_Y, COLOR_OBSTACLE, COLOR_OBSTACLE_BORDER, OBSTACLE_SIZE } from '../libs/constants';
 
 const Obstacle = ({ position }) => {
   const ref = useRef();  
 
   return (
     <mesh ref={ref} position={[position[0], OBSTACLE_TRANSLATE_Y, position[1]]}>
-      <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
+      <boxBufferGeometry attach='geometry' args={[OBSTACLE_SIZE, 1, OBSTACLE_SIZE]} />
       <meshStandardMaterial attach='material' color={COLOR_OBSTACLE} />
       <lineSegments>
-        <edgesGeometry attach="geometry" args={[new THREE.BoxGeometry(1, 1, 1)]} />
+        <edgesGeometry attach="geometry" args={[new THREE.BoxGeometry(OBSTACLE_SIZE, 1, OBSTACLE_SIZE)]} />
         <lineBasicMaterial attach="material" color={COLOR_OBSTACLE_BORDER} linewidth={2} />
       </lineSegments>
     </mesh>
